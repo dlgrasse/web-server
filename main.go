bcb9d68b3b41e4bca6e6cf26c502a811c4061ff9
Danny Grasse <Danny.Grasse@sas.com>
2024-01-02 16:31:43 -0500

    handling multiple requests per client and multiple clients, with virtual hosts; request-headers are being parsed out also

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	config := getConfig()
 
-	fmt.Printf("config=%v", config)
+	fmt.Printf("config=%v\n", config)
 
 	listener, err := net.Listen("tcp", fmt.Sprint(":", config.port))
 	if err != nil {
@@ -36,107 +36,148 @@ func main() {
 
 func handleConnection(conn net.Conn, config configMap) {
 	defer conn.Close()
-	
-	part := 0
-	methodBuf := bytes.NewBuffer(nil)
-	resourceBuf := bytes.NewBuffer(nil)
-	versionBuf := bytes.NewBuffer(nil)
-	charBuf := make([]byte, 1)
+
 	for {
-		n, err := conn.Read(charBuf)
-		if err != nil {
-			fmt.Printf("something happened reading from the connection %v\n", err.Error())
-			break
-		} else if n == 1 {
-			if charBuf[0] == 32 { // skip the char if a space
-				part++
-			} else if charBuf[0] == 13 { // ignore a CR
-				continue
-			} else if charBuf[0] == 10 { // stop of LF
+		errBreak := false
+		part := 0
+		eof := 0
+		firstLine := true
+		methodBuf := bytes.NewBuffer(nil)
+		resourceBuf := bytes.NewBuffer(nil)
+		versionBuf := bytes.NewBuffer(nil)
+		headerBuf := bytes.NewBuffer(nil)
+		headers := make(map[string]string)
+		charBuf := make([]byte, 1)
+		for { // we're just processing the first line; otherwise end after 2 consecutive CRLF or end of #Read
+			n, err := conn.Read(charBuf)
+			if err != nil {
+				fmt.Printf("something happened reading from the connection %v\n", err.Error())
+				errBreak = true
 				break
-			} else {
-				switch part {
-				case 0:
-					methodBuf.Write(charBuf[0:1])
-				case 1:
-					resourceBuf.Write(charBuf[0:1])
-				default:
-					versionBuf.Write(charBuf[0:1])
+			} else if n == 1 { // check char if 1 is read
+				if firstLine { // if we're still on the first line, parse out method/resource/version
+					if charBuf[0] == 13 { // CR which means we're at the end of the first line so ignore
+						continue
+					} else if charBuf[0] == 10 { // LF so bump the eof  and skip this first-line section
+						firstLine = false;
+						eof++
+					} else if charBuf[0] == 32 { // skip the char if a space or a CR (which bumps us off the first line)
+						part++
+					} else {
+						switch part {
+							case 0:
+								methodBuf.Write(charBuf[0:1])
+							case 1:
+								resourceBuf.Write(charBuf[0:1])
+							default:
+								versionBuf.Write(charBuf[0:1])
+						}
+					}
+				} else { // put all this stuff into the header buffer and exhause the request content
+					if charBuf[0] == 10 { // inc 'eof' after CRLF
+						eof++
+						if eof == 2 { // stop after 2 consecutive CRLF
+							break
+						}
+					} else if charBuf[0] == 13 { // at the end of the line, parse out content and add to our header map
+						headerSeparatorIdx := strings.Index(headerBuf.String(), ":")
+						if headerSeparatorIdx > 0 { // this will happen on 2nd CRLF
+							headerName := strings.ToLower(headerBuf.String()[0:headerSeparatorIdx])
+							headerValue := strings.TrimSpace(headerBuf.String()[headerSeparatorIdx+1:])
+							headers[headerName] = headerValue
+						}
+
+						headerBuf.Reset()
+					} else { // reset our eof counter if we read a non-CRLF char and add to our header buffer
+						eof = 0
+						headerBuf.Write(charBuf[0:1])
+					}	
 				}
+			} else {
+				fmt.Println("read 0 bytes from net.Conn")
+				break
 			}
-		} else {
+		}
+		if errBreak {
 			break
 		}
-	}
-	fmt.Printf("method '%v' resource '%v' version '%v'\n", methodBuf.String(), resourceBuf.String(), versionBuf.String())
-
-	var statusLine string
-
-	validMethod := checkMethod(methodBuf.String())
-	if !validMethod {
-		statusLine = getStatusLine(405)
-	}
-
-	resourceStr := resourceBuf.String()
-	if len(resourceStr) == 0 {
-		resourceStr = "/"
-	}
-
-	// determine if it's poiting to a virtual host or our root
-	var requestedResource string
-	resourceParts := strings.Split(resourceStr, "/")
-	virtualHost := "/"+resourceParts[1]
-	fmt.Printf("checking map %v for key %v\n", config.virtualHosts, virtualHost)
-	virtualRoot, okvh := config.virtualHosts[virtualHost]
-	if okvh {
-		virtualPath := resourceStr[len(virtualHost):]
-		requestedResource = fmt.Sprint(virtualRoot, virtualPath)
-	} else {
-		requestedResource = fmt.Sprint(config.root, resourceStr)
-	}
-	if string(requestedResource[len(requestedResource)-1]) == "/" {
-		requestedResource = requestedResource+"index.html"
-	}
-	var fileSize int64
-	var fileBytes []byte
-	okResp := false
-	fmt.Printf("reading resource %v...\n", requestedResource)
-
-	fileInfo, statErr := os.Stat(requestedResource)
-	if statErr != nil {
-		fmt.Printf("error stating file %v, %v", requestedResource, statErr.Error())
-		statusLine = getStatusLine(404)
-	} else {
-		fileSize = fileInfo.Size()
-		fmt.Printf("...size %v...\n", fileSize)
+		fmt.Printf("method '%v' resource '%v' version '%v'\n", methodBuf.String(), resourceBuf.String(), versionBuf.String())
+		for hKey,hVal := range headers {
+			fmt.Printf("...with header '%v'=%v\n", hKey, hVal)
+		}
+		contentLength, clOk := headers["content-length"]
+		if clOk {
+			fmt.Printf("content-length=%v\n", contentLength)
+		}
 	
-		fileBytes = make([]byte, fileSize)
-		file, openErr := os.Open(requestedResource)
-		if openErr != nil {
-			fmt.Printf("error opening file %v, %v", requestedResource, openErr.Error())
-			statusLine = getStatusLine(403)
+		var statusLine string
+	
+		validMethod := checkMethod(methodBuf.String())
+		if !validMethod {
+			statusLine = getStatusLine(405)
+		}
+	
+		resourceStr := resourceBuf.String()
+		if len(resourceStr) == 0 {
+			resourceStr = "/"
+		}
+	
+		// determine if it's poiting to a virtual host or our root
+		var requestedResource string
+		resourceParts := strings.Split(resourceStr, "/")
+		virtualHost := "/"+resourceParts[1]
+		fmt.Printf("checking map %v for key %v\n", config.virtualHosts, virtualHost)
+		virtualRoot, okvh := config.virtualHosts[virtualHost]
+		if okvh {
+			virtualPath := resourceStr[len(virtualHost):]
+			requestedResource = fmt.Sprint(virtualRoot, virtualPath)
 		} else {
-			numRead, readErr := file.Read(fileBytes)
-			if readErr != nil {
-				fmt.Printf("error reading from file %v, %v", requestedResource, readErr.Error())
+			requestedResource = fmt.Sprint(config.root, resourceStr)
+		}
+		if string(requestedResource[len(requestedResource)-1]) == "/" {
+			requestedResource = requestedResource+"index.html"
+		}
+		var fileSize int64
+		var fileBytes []byte
+		okResp := false
+		fmt.Printf("reading resource %v...\n", requestedResource)
+	
+		fileInfo, statErr := os.Stat(requestedResource)
+		if statErr != nil {
+			fmt.Printf("error stating file %v, %v\n", requestedResource, statErr.Error())
+			statusLine = getStatusLine(404)
+		} else {
+			fileSize = fileInfo.Size()
+			fmt.Printf("...size %v...\n", fileSize)
+		
+			fileBytes = make([]byte, fileSize)
+			file, openErr := os.Open(requestedResource)
+			if openErr != nil {
+				fmt.Printf("error opening file %v, %v\n", requestedResource, openErr.Error())
 				statusLine = getStatusLine(403)
 			} else {
-				okResp = true
-				statusLine = getStatusLine(200)
-
-				fmt.Printf("read %v bytes from file %v...", numRead, requestedResource)
-				fmt.Println(string(fileBytes))
+				numRead, readErr := file.Read(fileBytes)
+				if readErr != nil {
+					fmt.Printf("error reading from file %v, %v\n", requestedResource, readErr.Error())
+					statusLine = getStatusLine(403)
+				} else {
+					okResp = true
+					statusLine = getStatusLine(200)
+	
+					fmt.Printf("read %v bytes from file %v...\n", numRead, requestedResource)
+					// fmt.Println(string(fileBytes))
+				}
 			}
 		}
+	
+		conn.Write([]byte(statusLine))
+		if okResp {
+			conn.Write([]byte(fmt.Sprint("Content-Type: ", mimeForFile(requestedResource), "\r\n")))
+			conn.Write([]byte(fmt.Sprint("Content-Length: ", fileSize, "\r\n")))
+		}
+		conn.Write([]byte("\r\n"))
+		conn.Write(fileBytes)
 	}
-
-	conn.Write([]byte(statusLine))
-	if okResp {
-		conn.Write([]byte(fmt.Sprint("Content-Type: ", mimeForFile(resourceStr), "\r\n")))
-		conn.Write([]byte(fmt.Sprint("Content-Length: ", fileSize, "\r\n")))
-	}
-	conn.Write([]byte("\r\n"))
-	conn.Write(fileBytes)
 }
 
 func checkMethod(method string) (valid bool) {
